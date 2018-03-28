85eab8627763daf99eb213e87fa3869bbf9c26c2
boxingjellyfish <boxingjellyfish@noreply.com>
2018-03-27 22:14:36 -0300

    basic babylonjs

--- src/server/server.js
+++ src/server/server.js
@@ -8,6 +8,8 @@ const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, "../../index.html");
 
 const server = express()
+    .use(express.static("public"))
+    .use(express.static("src/client"))
     .use((req, res) => res.sendFile(INDEX))
     .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
